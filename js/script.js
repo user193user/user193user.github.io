1f78d9fbb377490eb43ac2df5b7a1c4c042de1a5
user193user <98658867+user193user@users.noreply.github.com>
2022-03-04 12:12:00 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,58 +13,112 @@ platformimg.src = "mat/platform.png"
 //global const
 const groundh = 10
 const firstherox = 10
-const ug = 0.4
-const dg = 0.1
-const pldx = -4 
-const jumph = canvas.clientHeight / 3
+const ug = 0.5
+const dg = 0.5
+const da = 20
+const pldx = -6 
+const jumph = canvas.clientHeight / 3 
 
 //global per
 let timer = 1
 let hero = {x: firstherox, y: 0, w: 0, h: 0, dx: 0, dy: 0}
 let platforms = []
+let maxspawny
+let minspawny
+let plobject = {
+    plmargin: {
+        right: 50,
+        left: 50,
+        top: 0,
+        bottom: 0
+    },
+    plcol: {
+        w: 0,
+        h: 0
+    }
+}
+
 let walls = []
 
-let jumpy = 0
+let fally = 0
 let isjumping = false
+let isfallingjerk = false
 let jumpscnt = 0
 
 let gameover = false
 //func
-canvas.addEventListener("mousedown", function(event) {
-    if(hero.dy == 0 || jumpscnt == 1)
+
+
+///////////////////////////////////////////swipe detectors
+let touchstartX = 0;
+let touchstartY = 0;
+let touchendX = 0;
+let touchendY = 0;
+
+canvas.addEventListener('mousedown', function(event) 
+{
+    touchstartX = event.offsetX;
+    touchstartY = event.offsetY;
+}, false);
+
+canvas.addEventListener('mouseup', function(event) 
+{
+    touchendX = event.offsetX;
+    touchendY = event.offsetY;
+    handleGesture();
+}, false); 
+
+function handleGesture() 
+{
+    if(Math.abs(touchendY - touchstartY) <= 10 && Math.abs(touchendX - touchstartX) <= 10)
+        swipeUp()
+    else if (Math.abs(touchendX - touchstartX) > Math.abs(touchendY - touchstartY)) 
+        if(touchendX > touchstartX)
+            console.log('Swiped right')
+        else
+            console.log('Swiped left')
+    else
+        if(touchendY < touchstartY)
+            swipeUp()
+        else
+            swipeDown()
+}
+
+function swipeUp()
+{
+    if(jumpscnt <= 1)
     {
         jumpscnt++
-        /*if(2*event.offsetY > 2*hero.y + hero.h && event.offsetY < hero.y + hero.h)
-        {
-            jumph = 0
-            jumpy = 0
-            isjumping = false
-        }
-        else
-        {
-            jumph = hero.y + hero.h - event.offsetY
-            if(jumph > 0)
-                jumph += 50
-            //if(hero.y - jumph < 0)
-            //    jumph = hero.y - 10
-            jumpy = event.offsetY
-            isjumping = true
-        }
-        if(jumph < 0)
-            hero.dy = 0
-        else
-            hero.dy = Math.round(Math.sqrt(2*ug*jumph))*/
         hero.dy = Math.round(Math.sqrt(2*ug*jumph))
+        fally = 0
         isjumping = true
     }
+}
 
-})
+function swipeDown()
+{
+    if(!isfallingjerk)
+    {
+        isfallingjerk = true
+        hero.dy = -da
+        isjumping = true
+        isfallingjerk = true
+        fally = hero.y + hero.h + 1
+    }
+}
+////////////////////////////////////////////
 
 platformimg.onload = function () 
 {
     hero.y = canvas.clientHeight - heroimg.height - groundh
     hero.w = heroimg.width
     hero.h = heroimg.height
+    plobject.plcol.w = platformimg.width*2
+    plobject.plcol.h = platformimg.height
+    plobject.plmargin.top = canvas.clientHeight / 8
+    plobject.plmargin.bottom = canvas.clientHeight / 8
+    minspawny = hero.h + 10
+    maxspawny = canvas.clientHeight - hero.h - groundh - 10
     game()
 }
 
@@ -75,7 +129,8 @@ function game()
         update()
     if(!gameover)
         render()
-    requestAnimFrame(game)
+    if(!gameover)
+        requestAnimFrame(game)
 }
 
 function update()
@@ -90,14 +145,57 @@ function update()
         }
         walls[i].x += walls[i].dx
     }
+    if(timer % 50 == 0)
+    {
+        let availablespawny = []
+        let spawnstep = 10
+        let tempy = maxspawny
+        console.log()
+        while(tempy >= minspawny)
+        {
+            let flag = true
+            for(let i in platforms)
+                if(platforms[i].x + platforms[i].w + plobject.plmargin.right > canvas.clientWidth)
+                    if(tempy >= platforms[i].y - plobject.plmargin.top && 
+                        tempy <= platforms[i].y + plobject.plmargin.bottom + platforms[i].h)
+                    {
+                        tempy -= spawnstep
+                        flag = false
+                        break
+                    }
+            if(flag)
+                availablespawny.push(tempy)
+            tempy -= spawnstep
+        }
+        if(availablespawny.length)
+        {
+            let spawny = availablespawny[randomInteger(0, availablespawny.length - 1)]
+            let spawnw = (Math.random() + 1)*plobject.plcol.w
+            platforms.push({
+                x: canvas.clientWidth, 
+                y: spawny, 
+                w: spawnw, 
+                h: plobject.plcol.h, 
+                dx: pldx})
+            if(Math.random() < 0.5)
+            {
+                walls.push({
+                    x: canvas.clientWidth + Math.random() * (spawnw - 50) + 25, 
+                    y: spawny - 50, 
+                    w: 25, 
+                    h: 50, 
+                    dx: pldx})
+            }            
+        }
+    }
     if(timer % 200 == 0)
     {
-        platforms.push({x: canvas.clientWidth, y: Math.floor(Math.random()*(canvas.height - 2*hero.h) + hero.h + 30), w: platformimg.width*2, h: platformimg.height, dx: pldx})
         timer = 0
-    }
-    if(timer % 150 == 0)
-    {
-        walls.push({x: canvas.clientWidth, y: Math.floor(Math.random()*(canvas.height - 2*hero.h) + hero.h + 30), w: 25, h: 100, dx: pldx})
+        walls.push({
+            x: canvas.clientWidth, 
+            y: canvas.clientHeight - 25, 
+            w: 100, 
+            h: 25, dx: pldx})
     }
     if(isjumping)
     {
@@ -109,14 +207,14 @@ function update()
         }
         else
         {
-            //platforms[0].y > jumpy && -hero.y - hero.h > -platforms[0].y && hero.y + hero.h - hero.dy > platforms[0].dy
             for(i in platforms)
-                if(isstandon(platforms[i]) && (jumph >= 0 || platforms[i].y >= jumpy))
+                if(isstandon(platforms[i]) && platforms[i].y > fally)
                 {
-                    hero.dy = 0
                     hero.y = platforms[i].y - hero.h
-                    isjumping = false
+                    hero.dy = 0
                     jumpscnt = 0
+                    isjumping = false
+                    isfallingjerk = false
                     return
                 }
             if(hero.y - hero.dy < canvas.clientHeight - heroimg.height - groundh)
@@ -130,9 +228,11 @@ function update()
                 hero.dy = 0
                 jumpscnt = 0
                 isjumping = false
+                isfallingjerk = false
             }
         }
     }
+    //проверка на падение с платформы
     let interf = false
     for(let i in platforms)
     {
@@ -140,6 +240,14 @@ function update()
         if(isstandon(platforms[i]))
             interf = true
     }
+    //падение с платформы
+    if(!interf && hero.y < canvas.clientHeight - heroimg.height - groundh && !isjumping)
+    {
+        isjumping = true
+        jumpscnt = 1
+        hero.dy = 0
+    }
+    //удаление
     let ind = 0
     while(ind < platforms.length)
     {
@@ -148,11 +256,19 @@ function update()
         else
             ind++
     }
-    if(!interf && hero.y < canvas.clientHeight - heroimg.height - groundh && !isjumping)
+    ind = 0
+    while(ind < walls.length)
     {
-        isjumping = true
-        hero.dy = 0
+        if(walls[ind].x + walls[ind].w < 0)
+            walls.splice(ind, 1)
+        else
+            ind++
     }
+}
+
+function randomInteger(min, max) 
+{
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function isstandon(platform)
@@ -185,14 +301,14 @@ let requestAnimFrame =
 (
     function () 
     {
-        return window.requestAnimationFrame ||
+        /*return window.requestAnimationFrame ||
             window.webkitRequestAnimationFrame ||
             window.mozRequestAnimationFrame ||
             window.oRequestAnimationFrame ||
-            window.msRequestAnimationFrame ||
-            function(callback)
+            window.msRequestAnimationFrame ||*/
+        return function(callback)
             {
-                window.setTimeout(callback, 1000/20)
+                window.setTimeout(callback, 1000/100)
             }
     }
 )()
